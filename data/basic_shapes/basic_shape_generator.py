7d645f9eb1a949f4427134cbf4b88ab0ed3b1f88
yinghansun <yinghansun2@gmail.com>
2022-09-30 20:29:36 +0800

    using function to create boxes

--- data/basic_shapes/basic_shape_generator.py
+++ data/basic_shapes/basic_shape_generator.py
@@ -22,30 +22,44 @@ stair2 = create_stairs(num_steps=5, length=0.225, width=1.3, height=0.2, label=T
 np.save(data_root_path + 'stair2.npy', stair2)
 
 
-for i in range(10):
-    name = 'box' + str(i) + '.npy'
-    length = np.random.rand()
-    width = np.random.rand()
-    height = np.random.rand()
+def generate_boxes_only(
+    num_boxes: int,
+    visualize: Optional[bool] = False,
+    save: Optional[bool] = False
+):
 
-    if np.random.rand() > 0.5:
-        length += 1
-    else:
-        length += 1.5
+    for i in range(num_boxes):
+        name = 'box' + str(i) + '.npy'
 
-    if np.random.rand() > 0.5:
-        width += 0.365
-    else:
-        width += 0.983
+        length = np.random.rand()
+        width = np.random.rand()
+        height = np.random.rand()
 
-    if np.random.rand() > 0.5:
-        height += 0.454
-    else:
-        height += 1.344
+        if np.random.rand() > 0.5:
+            length += 1
+        else:
+            length += 1.5
 
-    box_i = create_box(length=length, width=width, height=height, label=True)
-    angle = np.random.rand() * 3.1415926
-    box_i = rotate_pointlist(box_i, angle)
-    box_i = add_noise_pointlist(box_i, 0.005)
-    # visualize_pointlist(box_i)
-    np.save(data_root_path + name, box_i)
+        if np.random.rand() > 0.5:
+            width += 0.365
+        else:
+            width += 0.983
+
+        if np.random.rand() > 0.5:
+            height += 0.454
+        else:
+            height += 1.344
+
+        box_i = create_box(length=length, width=width, height=height, label=True)
+        angle = np.random.rand() * 3.1415926
+        box_i = rotate_pointlist(box_i, angle)
+        box_i = add_noise_pointlist(box_i, 0.005)
+
+        if visualize:
+            visualize_pointlist(box_i)
+        if save:
+            np.save(data_root_path + name, box_i)
+
+
+if __name__ == '__main__':
+    generate_boxes_only(1)

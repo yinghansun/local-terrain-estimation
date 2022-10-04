f119eb896d814bda80c248bfa99da16a976f6143
yinghansun <yinghansun2@gmail.com>
2022-10-04 20:20:50 +0800

    generate multiple boxes using one line

--- data/basic_shapes/basic_shape_generator.py
+++ data/basic_shapes/basic_shape_generator.py
@@ -1,19 +1,22 @@
 import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '../'))
 
 import numpy as np
 from basic_shapes_utils import *
+from data_augmentation_utils import *
 
 cur_path = os.path.dirname(os.path.abspath(__file__))
 data_root_path = cur_path + '/dataset/'
-# train_path = data_root_path + '/train/'
-# test_path = data_root_path + '/test/'
+train_path = data_root_path + '/train/'
+test_path = data_root_path + '/test/'
 
 if not os.path.exists(data_root_path):
     os.mkdir(data_root_path)
-# if not os.path.exists(train_path):
-#     os.mkdir(train_path)
-# if not os.path.exists(test_path):
-#     os.mkdir(test_path)
+if not os.path.exists(train_path):
+    os.mkdir(train_path)
+if not os.path.exists(test_path):
+    os.mkdir(test_path)
 
 stair1 = create_stairs(num_steps=7, length=0.128, width=1.2, height=0.125, label=True)
 np.save(data_root_path + 'stair1.npy', stair1)
@@ -53,7 +56,9 @@ def generate_boxes_only(
         box_i = create_box(length=length, width=width, height=height, label=True)
         angle = np.random.rand() * 3.1415926
         box_i = rotate_pointlist(box_i, angle)
-        box_i = add_noise_pointlist(box_i, 0.005)
+        box_i = add_noise_pointlist(box_i, 0.05)
+        box_i = remove_random_patches(box_i)
+        box_i = add_random_clusters(box_i)
 
         if visualize:
             visualize_pointlist(box_i)
@@ -62,4 +67,4 @@ def generate_boxes_only(
 
 
 if __name__ == '__main__':
-    generate_boxes_only(1)
+    generate_boxes_only(100, save=False)
